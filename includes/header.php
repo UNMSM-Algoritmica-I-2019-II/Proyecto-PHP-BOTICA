d2770302fdd20c0e751ffd3597e03de3216afd70
cruzmanuelar <49384686+cruzmanuelar@users.noreply.github.com>
2019-11-28 21:15:14 -0500

    header

---
+++ includes/header.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BOTICA FISI</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--FONT AWESOME 5-->
+    <link rel="stylesheet" href="pagina.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <style>
+        .noventas {
+            text-align: center;
+            padding: 5px;
+            margin: 0px;
+            font-size: large;
+            font-family: "Consolas", monospace;
+            list-style: none;
+            color: black;
+            
+        }
+        .fondo{
+            background-color:#bdbdbd;
+        }
+
+        .titulo{
+            padding: 15px;
+            text-align: center;
+
+        }
+
+        #peque{
+            font-size: 20px;
+        }
+        img {
+            width: 700px;
+            display: block;
+            margin: 20px auto;
+            border: 3px solid black;
+            background-color: #948f8f;
+        }
+        .dir{
+            font-size: 40px;
+        }
+        footer {
+            position: absolute;
+            bottom: 0px; 
+            height: 40px;
+            border: 1px solid #000;
+            background: #0d0c0c;
+            text-align: center;
+            color: #fff;
+            width: 100%;
+            padding: 6px;
+            
+        }
+        .define{
+            background-color: #0d0c0c;
+        }
+    </style>
+
+</head>
+<body>
+
+<nav class="menu">
+    <div class="containerh">
+        <a href="index.php" class="navbar-brand">MENU PRINCIPAL</a>
+
+    </div>
+    <div class="containerh">
+        <a href="producto.php" class="navbar-brand">PRODUCTOS</a>
+
+    </div>
+    <div class="containerh">
+        <a href="cliente.php" class="navbar-brand">CLIENTES</a>
+
+    </div>
+    <div class="containerh">
+        <a href="ventas.php" class="navbar-brand">VENTA</a>
+
+    </div>
+    <div class="containerh">
+        <a href="ingreso_sistema.php" class="navbar-brand">CERRAR SESION</a>
+
+    </div>
+
+</nav>
